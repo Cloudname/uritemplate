1ed3650542b1d798a4b6081f370c41dd157cc22b
Vidar Larsen <vlarsen@comoyo.com>
2011-11-14 12:55:03 +0100

    init

---
+++ src/main/java/org/cloudname/uritemplate/App.java
@@ -0,0 +1,13 @@
+package org.cloudname.uritemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
